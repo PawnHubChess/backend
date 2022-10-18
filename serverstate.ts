bbe10cb759f97ca034070709cc600de214db8658
Florian König <koenidv@gmail.com>
2022-10-18 19:29:55 +0200

    Prevent non-unique host ids

--- serverstate.ts
+++ serverstate.ts
@@ -3,5 +3,13 @@ import { Game } from "./game.ts";
 const games: Array<Game> = [];
 
 export function createHost(hostId: string, ws: WebSocket) {
+  if (getGameByHostid(hostId) !== null) {
+    throw new Error("Host already exists");
+  }
+
   games.push(new Game(hostId, ws));
+}
+
+export function getGameByHostid(id: string): Game | null {
+  return games.filter((game) => game.hostId === id)[0] || null;
 }
