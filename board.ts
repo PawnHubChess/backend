e08d78f217e6ad7062521289094a98a2a62f0724
Florian König <koenidv@gmail.com>
2022-10-20 15:45:55 +0200

    Set up board class

--- board.ts
+++ board.ts
@@ -1,3 +1,11 @@
+import { ChessPiece } from "./ChessPieces/_ChessPiece.ts"
+
 export class Board {
+
+    state: (ChessPiece | null)[][] = new Array(8).fill(new Array(8).fill(null));
+
+    constructor() {
+        this.state = [[]]
+    }
 
 }
