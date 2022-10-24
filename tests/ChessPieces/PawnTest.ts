7952d414177231e7ddcdc5ebd532ef848f16bbf9
Florian König <koenidv@gmail.com>
2022-10-24 16:23:50 +0200

    PawnTest Restrict 3 fields move

--- tests/ChessPieces/PawnTest.ts
+++ tests/ChessPieces/PawnTest.ts
@@ -29,6 +29,14 @@ Deno.test("black forward 2 from B7", () => {
     assertEquals(pawn.validateMove(from, to, board), true)  
 })
 
+Deno.test("black forward 3 from B7", () => {
+    const pawn = new Pawn(false);
+    const board = new Board();
+    const from = new BoardPosition("B7");
+    const to = new BoardPosition("B4");
+    assertEquals(pawn.validateMove(from, to, board), false)
+})
+
 Deno.test("black forward 1 from B6", () => {
     const pawn = new Pawn(false);
     const board = new Board();
@@ -87,6 +95,14 @@ Deno.test("white forward 2 from B2", () => {
     assertEquals(pawn.validateMove(from, to, board), true)
 })
 
+Deno.test("white forward 2 from B3", () => {
+    const pawn = new Pawn(true);
+    const board = new Board();
+    const from = new BoardPosition("B3");
+    const to = new BoardPosition("B5");
+    assertEquals(pawn.validateMove(from, to, board), false)
+})
+
 Deno.test("white backwards 1 from B3", () => {
     const pawn = new Pawn(true);
     const board = new Board();
@@ -102,7 +118,6 @@ Deno.test("white backwards 2 from B3", () => {
     const to = new BoardPosition("B1");
     assertEquals(pawn.validateMove(from, to, board), false)  
 })
-
 
 // todo diagonal moves
 // todo collisions
