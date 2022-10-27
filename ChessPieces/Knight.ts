7b120a8e815a4a4f94e5862a9d9b5c5b18fca55b
Florian König <koenidv@gmail.com>
2022-10-27 19:45:13 +0200

    Implement Knight according to tests

--- ChessPieces/Knight.ts
+++ ChessPieces/Knight.ts
@@ -3,9 +3,16 @@ import { BoardPosition } from "../BoardPosition.ts";
 import { ChessPiece } from "./_ChessPiece.ts";
 
 export class Knight extends ChessPiece {
-
   validateMove(from: BoardPosition, to: BoardPosition, board: Board): boolean {
+    // L shaped move to the side or top
+    if (
+      (Math.abs(from.x - to.x) !== 2 || Math.abs(from.y - to.y) !== 1) &&
+      (Math.abs(from.x - to.x) !== 1 || Math.abs(from.y - to.y) !== 2)
+    ) return false;
+
+    // No collisions with same color
+    if (board.get(to)?.isWhite === this.isWhite) return false;
+
     return true;
   }
-  
 }
