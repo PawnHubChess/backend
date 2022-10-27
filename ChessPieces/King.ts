3f6b73a7f9378ef1e9dea668ad52a9ef913a3a91
Florian König <koenidv@gmail.com>
2022-10-27 19:06:17 +0200

    Implement King move validation

--- ChessPieces/King.ts
+++ ChessPieces/King.ts
@@ -3,9 +3,16 @@ import { BoardPosition } from "../BoardPosition.ts";
 import { ChessPiece } from "./_ChessPiece.ts";
 
 export class King extends ChessPiece {
-
   validateMove(from: BoardPosition, to: BoardPosition, board: Board): boolean {
+    // Maximum 1 step
+    if (
+      Math.abs(from.x - to.x) > 1 ||
+      Math.abs(from.y - to.y) > 1
+    ) return false;
+
+    // No collisions with same color
+    if (board.get(to)?.isWhite === this.isWhite) return false;
+
     return true;
   }
-  
 }
