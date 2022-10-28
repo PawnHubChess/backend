bc1384235512fe29fe526279a883c251747c1183
Florian König <koenidv@gmail.com>
2022-10-28 13:24:38 +0200

    Implement board initialization

--- Board.ts
+++ Board.ts
@@ -1,5 +1,11 @@
 import { BoardPosition } from "./BoardPosition.ts";
 import { ChessPiece } from "./ChessPieces/_ChessPiece.ts";
+import { Pawn } from "./ChessPieces/Pawn.ts";
+import { Rook } from "./ChessPieces/Rook.ts";
+import { Knight } from "./ChessPieces/Knight.ts";
+import { Bishop } from "./ChessPieces/Bishop.ts";
+import { King } from "./ChessPieces/King.ts";
+import { Queen } from "./ChessPieces/Queen.ts";
 
 export class Board {
   state: (ChessPiece | null)[][];
@@ -23,6 +29,59 @@ export class Board {
   }
 }
 
-const initBoard = () => new Array(8).fill(null).map(() => new Array(8).fill(null));
+function initBoard(): (ChessPiece | null)[][] {
+  return new Array(8)
+    .fill(null)
+    .map((_, row) => {
+      switch (row) {
+        case 0:
+          return initFirstRow(true);
+        case 1:
+          return initPawnRow(true);
+        case 7:
+          return initFirstRow(false);
+        case 6:
+          return initPawnRow(false);
+        default:
+          return initEmptyRow();
+      }
+    });
+}
 
-export const initEmptyBoard = () => new Array(8).fill(null).map(() => new Array(8).fill(null));
+const initPawnRow = (isWhite: boolean): Pawn[] => {
+  return new Array(8)
+    .fill(null)
+    .map(() => new Pawn(isWhite));
+};
+
+const initFirstRow = (isWhite: boolean): (ChessPiece | null)[] => {
+  // Columns are counted H->A, thus King is on 3 (E) and Queen is on 4 (D)
+  return new Array(8)
+    .fill(null)
+    .map((_, column) => {
+      switch (column) {
+        case 0:
+        case 7:
+          return new Rook(isWhite);
+        case 1:
+        case 6:
+          return new Knight(isWhite);
+        case 2:
+        case 5:
+          return new Bishop(isWhite);
+        case 3:
+          return new King(isWhite);
+        case 4:
+          return new Queen(isWhite);
+        default:
+          return null;
+      }
+    });
+};
+
+const initEmptyRow = (): null[] => {
+  return new Array(8).fill(null);
+};
+
+export const initEmptyBoard = (): null[][] =>
+  new Array(8).fill(null).map(() => new Array(8).fill(null));
