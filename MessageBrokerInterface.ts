099be19ef1aa61ff9bbe718d287d9ff474872e03
Florian König <32238636+koenidv@users.noreply.github.com>
2022-11-29 15:20:26 +0100

    Create and destroy queues

--- MessageBrokerInterface.ts
+++ MessageBrokerInterface.ts
@@ -24,3 +24,15 @@ export async function publish(queue: string, message: any) {
     new TextEncoder().encode(JSON.stringify(message)),
   );
 }
+
+// MUST be awaited BEFORE publishing or subscribing
+export async function createQueue(queue: string) {
+  const channel = await getChannel();
+  await channel.declareQueue({ queue: queue });
+}
+
+// This will DELETE ALL messages in the queue
+export async function destroyQueue(queue: string) {
+  const channel = await getChannel();
+  await channel.deleteQueue({ queue: queue });
+}
