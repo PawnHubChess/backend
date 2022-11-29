3df2ef7c104c41a07761e21d1f8dee8bf4a7cfc0
Florian König <32238636+koenidv@users.noreply.github.com>
2022-11-29 15:09:28 +0100

    Publish function

--- MessageBrokerInterface.ts
+++ MessageBrokerInterface.ts
@@ -1,4 +1,5 @@
 import { getChannel } from "./MessageBrokerChannel.ts";
+
 export async function subscribe(
   queue: string,
   callback: (message: any) => void,
@@ -12,5 +13,14 @@ export async function subscribe(
       callback(json);
       await channel.ack({ deliveryTag: args.deliveryTag });
     },
+  );
+}
+
+export async function publish(queue: string, message: any) {
+  const channel = await getChannel();
+  await channel.publish(
+    { routingKey: queue },
+    { contentType: "application/json" },
+    new TextEncoder().encode(JSON.stringify(message)),
   );
 }
